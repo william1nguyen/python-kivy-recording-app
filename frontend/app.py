bbfad4a8bdabb6dc2513d5219c8b935bf5764387
natalieconan <natalieconan17@gmail.com>
2023-12-02 12:51:26 +0700

    update main screen

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
-from controller import login, signup, main, option
+from controller import login, signup, main
 from kivy.core.window import Window
-from kivy.config import ConfigParser
 
 WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 700
@@ -25,17 +24,14 @@ class App(App):
         self.title = "Recording App"
         window_manager = WindowManager()
 
-        # login_window = login.LoginWindow()
-        # window_manager.add_widget(login_window)
+        login_window = login.LoginWindow()
+        window_manager.add_widget(login_window)
 
-        # signup_window = signup.SignupWindow()
-        # window_manager.add_widget(signup_window)
+        signup_window = signup.SignupWindow()
+        window_manager.add_widget(signup_window)
 
-        option_window = option.OptionWindow()
-        window_manager.add_widget(option_window)
-
-        # main_window = main.MainWindow()
-        # window_manager.add_widget(main_window)
+        main_window = main.MainWindow()
+        window_manager.add_widget(main_window)
 
         return window_manager
 
--- frontend/controller/main.py
+++ frontend/controller/main.py
@@ -1,12 +1,56 @@
+import os
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+from kivy.properties import NumericProperty, ObjectProperty
 
 Builder.load_file("./views/components/rounded_button.kv")
 Builder.load_file("./views/main.kv")
 
+dirname = os.path.dirname(__file__).replace("/controller", "")
+
 
 class MainWindow(Screen):
+    MICRO_ON_IMG_SRC = os.path.join(dirname, "views/assets/micro.jpeg")
+    MICRO_OFF_IMG_SRC = os.path.join(dirname, "views/assets/micro.png")
+    REWIND_BUTTON_IMG_SRC = os.path.join(dirname, "views/assets/rewind-button.png")
+    FORWARD_BUTTON_IMG_SRC = os.path.join(dirname, "views/assets/forward-button.png")
+    RECORD_ON_IMG_SRC = os.path.join(dirname, "views/assets/record_on.png")
+    RECORD_OFF_IMG_SRC = os.path.join(dirname, "views/assets/record_off.png")
+
     is_recorded = False
+    audio = ObjectProperty()
+    time = NumericProperty(0)
 
     def __init__(self, **kw):
         super().__init__(**kw)
+
+    def turn_on_signal(self):
+        self.ids.record_signal.source = self.RECORD_ON_IMG_SRC
+
+    def turn_off_signal(self):
+        self.ids.record_signal.source = self.RECORD_OFF_IMG_SRC
+
+    def turn_on_mic(self):
+        self.ids.micro.source = self.MICRO_ON_IMG_SRC
+
+    def turn_off_mic(self):
+        self.ids.micro.source = self.MICRO_OFF_IMG_SRC
+
+    def start_recording(self):
+        self.audio.start()
+        self.turn_on_signal()
+        self.turn_on_mic()
+        self.is_recorded = True
+
+    def stop_recording(self):
+        self.audio.stop()
+        self.turn_off_signal()
+        self.turn_off_mic()
+        self.is_recorded = False
+
+    def play_recording(self):
+        state = self.audio.state
+        if state == "playing":
+            self.audio.stop()
+        else:
+            self.audio.play()
